3c8f6ad21e4709e3009dd6749807065ff5a24543
Ronald Cabrera González <ronald.cabrera.gonzalez@gmail.com>
2022-11-08 20:00:45 -0400

    1.0.2 - Server Mixin & Langs
    * Added Spanish Translation (JoseObed1).
    * Added Chinese Translation (enotswolgender).
    * Added Missing Server Mixins Parameters.
    * Used Proper Equip Method to Drop on Death.

--- src/main/java/net/pevori/queencats/entity/custom/HumanoidCatEntity.java
+++ src/main/java/net/pevori/queencats/entity/custom/HumanoidCatEntity.java
@@ -1,10 +1,12 @@
 package net.pevori.queencats.entity.custom;
 
 import net.minecraft.server.level.ServerLevel;
-import net.minecraft.world.entity.AgeableMob;
-import net.minecraft.world.entity.EntityType;
-import net.minecraft.world.entity.TamableAnimal;
+import net.minecraft.world.damagesource.DamageSource;
+import net.minecraft.world.entity.*;
 import net.minecraft.world.level.Level;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.entity.living.LivingDeathEvent;
+import net.minecraftforge.event.entity.living.LivingDropsEvent;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.Formattable;
