f54210681f2badea591ee277c3a380ae961bd858
Ronald Cabrera González <ronald.cabrera.gonzalez@gmail.com>
2024-01-31 22:07:12 -0400

    1.0.6 - Fixing Mixins
    * Refactorized the Queen Cow Mixin so it works properly (was producing crashes).

--- src/main/java/net/pevori/queencats/mixin/QueenCowMixin.java
+++ src/main/java/net/pevori/queencats/mixin/QueenCowMixin.java
@@ -6,16 +6,18 @@ import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.animal.Animal;
 import net.minecraft.world.entity.animal.Cow;
+import net.minecraft.world.entity.animal.Wolf;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.Level;
 import net.pevori.queencats.entity.ModEntityTypes;
-import net.pevori.queencats.entity.custom.HumanoidCowEntity;
-import net.pevori.queencats.entity.custom.PrincessCowEntity;
-import net.pevori.queencats.entity.custom.QueenCowEntity;
+import net.pevori.queencats.entity.custom.*;
 import net.pevori.queencats.entity.variants.HumanoidCowVariant;
 import net.pevori.queencats.item.ModItems;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(Cow.class)
 public abstract class QueenCowMixin extends Animal {
@@ -23,9 +25,8 @@ public abstract class QueenCowMixin extends Animal {
         super(pEntityType, pLevel);
     }
 
-    //This Mixin injection is to make the Cow turn into a Humanoid Cow when fed with a Kemomimi Potion.
-    @Override
-    public InteractionResult mobInteract(Player player, InteractionHand hand) {
+    @Inject(method = "mobInteract", at = @At("HEAD"))
+    protected void injectInteractMethod(Player player, InteractionHand hand, CallbackInfoReturnable info) {
         Cow cow = ((Cow) (Object) this);
         Level level = cow.getCommandSenderWorld();
         Item usedItem = player.getItemInHand(hand).getItem();
@@ -39,17 +40,12 @@ public abstract class QueenCowMixin extends Animal {
                 player.getItemInHand(hand).shrink(1);
             }
 
-            if (cow.isBaby()) {
-                PrincessCowEntity princessCow = ModEntityTypes.PRINCESS_COW.get().create(level);
-                spawnHumanoidCow(princessCow, cow, player);
-            }
-            else{
-                QueenCowEntity queenCow = ModEntityTypes.QUEEN_COW.get().create(level);
-                spawnHumanoidCow(queenCow, cow, player);
-            }
-        }
+            HumanoidCowEntity humanoidCowEntity = cow.isBaby()
+                    ? ModEntityTypes.PRINCESS_COW.get().create(level)
+                    : ModEntityTypes.QUEEN_COW.get().create(level);
 
-        return super.mobInteract(player, hand);
+            spawnHumanoidCow(humanoidCowEntity, cow, player);
+        }
     }
 
     public void spawnHumanoidCow(HumanoidCowEntity humanoidCowEntity, Cow cow, Player player){
